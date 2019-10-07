47b380e79a1af638d8cf3500ed1e8fd4343ac412
attila5287 <42498753+attila5287@users.noreply.github.com>
2019-10-07 00:53:44 -0600

    fx for second crash

--- piri4tableau_data_mapper_app/app.py
+++ piri4tableau_data_mapper_app/app.py
@@ -1,11 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 import os
 from flask import (Flask, render_template, redirect, jsonify, request)
-from flask_pymongo import PyMongo
 import json
-from Pay_stub import Pay_stub, Employee_form_data, ModGeneratedPayStubFrom
 from flask_wtf import FlaskForm  
-from forms import ContactForm, Timesheet, Tim3sheet, EmployeeForm 
 app = Flask(__name__)
 flask_debug = False
 app.config['FLASK_DEBUG'] = flask_debug
